8473650c0274b4d0020111c29b7065a7e5cab364
JC <JC@DESKTOP-Q9KNP2H>
2023-09-30 11:16:28 -0600

    msg

---
+++ Sesion1/src/main/java/packageSesion2/sesion1.java
@@ -0,0 +1,18 @@
+package packageSesion2;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class sesion1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+		
+
+	}
+
+}
